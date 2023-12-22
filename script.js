3ad15163aaef903a035a3f8c4f22b27e8deaca36
Dariya Vedernikova <daria-vedernikova08@yandex.ru>
2023-12-22 18:26:02 +0300

    feat: add function delete card

--- script.js
+++ script.js
@@ -4,26 +4,45 @@ let heroes = [
     ["Crystal", "Supp"]
 ];
 
+const handleDelete = (cardButton) => {
+    const hero = cardButton.dataset.hero;
+    const classes = cardButton.dataset.classes;
+    const cardId = hero.split(' ').join("") + classes;
+    heroes = heroes.filter(card => {
+        return cardId !== card[0].split(' ').join("") + card[1];
+    });
+    displayHeroes();
+
+}
+
 const heroesContainer = document.getElementById("heroesContainer");
 
 function displayHeroes() {
     // Очищаем текущее содержимое
     heroesContainer.innerHTML = "";
 
-    // С помощью цикла for of проходимся по массиву массивов героев
-    // с помощью метода деструктуризации массивов получаем переменные hero и classes
-    for (let [hero, classes] of heroes) {
-        // Создаём элемент, в который будем добавлять информацию о герое
-        let heroDiv = document.createElement("div");
-        // добавляем класс для возможности стилизации эдемента
-        heroDiv.classList.add("card");
 
-        // Записываем в созданный элемент разметку, подставляя необходимые данные
-        heroDiv.innerHTML = `<h3>${hero}</h3><p>${classes}</p>`;
+    for (let i = 0; i < heroes.length; i += 3) {
+        const heroesSlice = heroes.slice(i, i + 3);
+        const row = document.createElement("div")
+        row.classList.add("row");
+        heroesContainer.appendChild(row);
+        // С помощью цикла for of проходимся по массиву массивов героев
+        // с помощью метода деструктуризации массивов получаем переменные hero и classes
+        for (const [hero, classes] of heroesSlice) {
+            // Создаём элемент, в который будем добавлять информацию о герое
+            let heroDiv = document.createElement("div");
+            // добавляем класс для возможности стилизации эдемента
+            heroDiv.classList.add("card");
 
-        // Добавляем карточку героя в контейнер
-        heroesContainer.appendChild(heroDiv);
+            // Записываем в созданный элемент разметку, подставляя необходимые данные
+            heroDiv.innerHTML = `<h3>${hero}</h3><p>${classes}</p><button data-hero="${hero}" data-classes="${classes}" onclick="handleDelete(this)">Delete</button>`;
+
+            // Добавляем карточку героя в контейнер
+            row.appendChild(heroDiv);
+        }
     }
+
 }
 
 // если в начале работы приложения массив heroes имеет данные, то выводим их на экран
